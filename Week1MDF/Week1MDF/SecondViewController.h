4dd22e855296189141403c1338e87abad9a0a280
Cameron Mozie <cam25@fullsail.edu>
2013-04-08 08:53:55 -0400

    second view controller added

---
+++ Week1MDF/Week1MDF/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Week1MDF
+//
+//  Created by Cameron Mozie on 4/8/13.
+//  Copyright (c) 2013 Cameron Mozie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end

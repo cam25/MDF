30986da345eb127a1e2d1e7ca32b655eb9cb559b
Cameron Mozie <cam25@fullsail.edu>
2013-04-08 10:35:44 -0400

    TableView Entered

--- Week1MDF/Week1MDF/ViewController.m
+++ Week1MDF/Week1MDF/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad
 {
+    
+    hairTypes = [[NSArray alloc] initWithObjects:@"Maylasian Hair",@"Brazilian Hair",@"African Hair",@"Indian Hair",@"Peruvian", @"Cambodian Hair",@"Russian Hair",@"Chinese Hair",@"European Hair",@"Filipino Hair",@"Regular Wigs",@"LaceFront Wigs",@"Glueless Wigs",@"Full Lace Wigs", @"Kids Lace Wigs",@"Human Hair Wigs", @"Hand Tied Wigs",@"Synthetic Wigs", @"Actress Wigs", @"Entertainer Wigs", nil];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -25,5 +27,45 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if(section == 0){
+        
+        return  10;
+        
+    }
+    else if (section == 1){
+        return 10;
+    }
+    return 0;
+}
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView2 cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    
+    NSInteger actualRow = 0;
+    actualRow = (indexPath.section * 10) + indexPath.row;
+  
+        
+        
+         cell.textLabel.text = (NSString*)[hairTypes objectAtIndex:actualRow];
+   
+   
+    
+
+    return cell;
+    
+}
 @end

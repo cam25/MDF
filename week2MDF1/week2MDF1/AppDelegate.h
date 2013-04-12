4bb8f52e2d0219639da2c70b498d29188779e885
Cameron Mozie <cam25@fullsail.edu>
2013-04-12 13:33:23 -0400

    Initial Commit

---
+++ week2MDF1/week2MDF1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  week2MDF1
+//
+//  Created by Cameron Mozie on 4/12/13.
+//  Copyright (c) 2013 Cameron Mozie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ week2MDF1/week2MDF1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  week2MDF1
+//
+//  Created by Cameron Mozie on 4/12/13.
+//  Copyright (c) 2013 Cameron Mozie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

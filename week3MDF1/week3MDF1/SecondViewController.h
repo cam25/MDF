1c46dd9bdf9c5ee731d442df4f7f3009e78c2ed2
Cameron Mozie <cam25@fullsail.edu>
2013-04-18 19:33:10 -0400

    Initial commit

---
+++ week3MDF1/week3MDF1/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  week3MDF1
+//
+//  Created by Cameron Mozie on 4/18/13.
+//  Copyright (c) 2013 Cameron Mozie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ week3MDF1/week3MDF1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  week3MDF1
+//
+//  Created by Cameron Mozie on 4/18/13.
+//  Copyright (c) 2013 Cameron Mozie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
